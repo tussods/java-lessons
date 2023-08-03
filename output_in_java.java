7c947a091ee5383a1b5b0f73c3a26fc7f05fffd6
tussods <tussods@gmail.com>
2023-08-03 23:24:16 +0530

    print function

---
+++ output_in_java.java
@@ -0,0 +1,8 @@
+public class output_in_java{
+    public static void main(String args[]) {
+        System.out.print("Hello, World!");
+        System.out.print("Hello, World!\n");
+        System.out.println("Hello, World!");
+        System.out.print("Hello, World!");
+    }
+}
