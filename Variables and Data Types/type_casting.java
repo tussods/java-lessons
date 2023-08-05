6014b230f36209a2f080f55193e3ea0566707d27
tussods <tussods@gmail.com>
2023-08-05 19:34:36 +0530

    type casting

---
+++ Variables and Data Types/type_casting.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class type_casting {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float a = 25.12f;
+        int b = (int)a;
+        System.out.println(b);
+    }
+    
+}
