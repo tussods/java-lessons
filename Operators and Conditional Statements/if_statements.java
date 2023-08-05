48f38ee0118f29b75446c4374baa7b480268a8d5
tussods <tussods@gmail.com>
2023-08-05 19:36:17 +0530

    if statements

---
+++ Operators and Conditional Statements/if_statements.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class if_statements {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        if(age>=18){
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are a minor.");
+        }
+
+    }
+    
+}
