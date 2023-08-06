39b1640400ff90a3ff84ef24b2a7174769ab5ae4
tussods <tussods@gmail.com>
2023-08-06 20:17:40 +0530

    using while loop to print

---
+++ Loops/print_1to10.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class print_1to10 {
+    public static void main(String[] args){
+
+        int counter = 1;
+        while(counter<=10){
+            System.out.println(counter);
+            counter++;
+        }
+    }
+    
+}
