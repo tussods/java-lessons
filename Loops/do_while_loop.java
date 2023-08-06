be080d33da0af585854a88083e7ba99006aa8f98
tussods <tussods@gmail.com>
2023-08-06 21:40:24 +0530

    do-while loop

---
+++ Loops/do_while_loop.java
@@ -0,0 +1,16 @@
+public class do_while_loop {
+    public static void main(String[] args){
+
+        int counter = 1;
+        
+        do{
+            System.out .println("Hello World!");
+            counter++;
+        }
+
+        while(counter<=10);
+
+
+    }
+    
+}
