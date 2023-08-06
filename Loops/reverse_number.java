df8dea2b29a5b2157ca277a5eca5f62f5c40ebd6
tussods <tussods@gmail.com>
2023-08-06 21:22:54 +0530

    reverse and store a number

---
+++ Loops/reverse_number.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class reverse_number {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        int reverse_number = 0;
+
+        System.out.print("The reverse of the entered number is: ");
+
+        while(number>1){
+            reverse_number = reverse_number*10 + (number%10);
+
+            number /= 10;
+            // number = number/10;
+        }
+
+        System.out.print("The reverse number is stored in a variable and is: " + reverse_number);
+    }    
+}
