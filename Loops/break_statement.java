065118f13251ab16fde0570e1c686b090f340ca7
tussods <tussods@gmail.com>
2023-08-06 22:11:41 +0530

    break statement

---
+++ Loops/break_statement.java
@@ -0,0 +1,14 @@
+public class break_statement {
+    public static void main(String[] args){
+        for(int i=1; i<=5; i++){
+
+            if(i==4){
+                break;
+            }
+
+            System.out.println(i);
+        
+        }
+    
+    }
+}
